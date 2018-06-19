2a294dbddc18467025604e63aae360c32c7d0f5e
Roy Mizrahi <34283540+RoyGuf@users.noreply.github.com>
2018-06-19 14:35:29 +0300

    Connect Redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,59 @@
 import React, {PropTypes} from 'react';
-
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import CourseList from './CourseList';
+import * as courseActions from '../../actions/courseActions';
+import {browserHistory} from 'react-router';
+ 
 class CoursesPage extends React.Component{
+    constructor(props, context){
+        super(props, context);
+
+        this.state = {
+            course: {title: ''}
+        };
+
+        this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+    }
+
+
+    redirectToAddCoursePage() {
+        browserHistory.push('/course');
+    }
+
     render(){
         return(
             <div className='jumotron'>
                 <h1>Courses</h1>
+                <CourseList courses={this.props.courses} />
+                <h2>Add Course</h2>
+                <input
+                    type='submit'
+                    value='Add Course'
+                    className='btn btn-primary'
+                    onClick={this.redirectToAddCoursePage} />
             </div>
         );
     }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+    
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownProps){
+    return{
+        courses: state.courses
+    };
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+      actions: bindActionCreators(courseActions, dispatch)  
+    };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import  'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import {Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/style.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { loadCourses } from './actions/courseActions';
+import { loadAuthors } from './actions/authorActions';
+
+const store = configureStore();
+store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
-    ///history for back/forward buttons in the browser to work correctly 
-    <Router history={browserHistory} routes={routes} />,
+    ///history for back/forward buttons in the browser to work correctly
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
  
